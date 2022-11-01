cfc6fad3bc3bdc7031b00ebbf8785ef09bfcae6d
Ashamovskiy <476136@bk.ru>
2022-11-01 23:19:52 +0300

    Solving the Task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,51 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let d = (b**2) - 4 * a * c;
+  if (d === 0) {
+    arr[0] = -b / ( 2 * a )
+  } else if ( d > 0 ) {
+    arr[0] = (-b + (Math.sqrt(d)) ) / ( 2 * a );
+    arr[1] = (-b - (Math.sqrt(d)) ) / ( 2 * a );
+
+  } 
   // код для задачи №1 писать здесь
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let arr = {
+    percent:percent, 
+    contribution:contribution, 
+    amount:amount,
+  }
+  for ( let key of Object.keys(arr)) {
+      if (arr[key] === '') {
+      let span = window.mortageResult;
+      let placeholder = document.getElementById(`${key}`)
+      span.textContent = `Параметр ${placeholder.getAttribute('placeholder')}  не заполнен`
+      return span.textContent
+    } else if (!isFinite(arr[key])) {
+      let span = window.mortageResult;
+      let placeholder = document.getElementById(`${key}`)
+      span.textContent = `Параметр ${placeholder.getAttribute('placeholder')} содержит неправильное значение ${arr[key]}`
+      return span.textContent
+    }
+  };
+  
+  let loanBody = parseInt(amount) - parseInt(contribution);
+  let dateNow = Date.parse(new Date());
+  let dateLoan = Date.parse(date);
+  let loanTerm = Math.round((dateLoan - dateNow) / 1000 / 60 / 60 / 24 / 30.43);
+  console.log(loanTerm)
+  let interestRatePerMoonth = parseInt(percent) / 12 / 100;
+  let loanPayment = (loanBody * (interestRatePerMoonth * (1 + interestRatePerMoonth) ** loanTerm /((1 + interestRatePerMoonth) ** loanTerm - 1)));
 
+  totalAmount = Number((loanPayment * loanTerm).toFixed(2));
+  
   // код для задачи №2 писать здесь
-
+  console.log(typeof(totalAmount));
   return totalAmount;
 }
+
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,23 @@
-// Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = 100;
+  max = -100;
+  sum = 0;
+  avg = 0;
+  console.log(arr)
+
+  for (let i = 0; i < arr.length; i++ ) {
+    if (max < arr[i]) {
+      max = arr[i];
+    };
+    if ( min > arr[i]) {
+      min = arr[i];
+    };
+
+    sum = sum + arr[i];
+    avg = Number((sum / arr.length).toFixed(2))
+  }; 
 
   return { min: min, max: max, avg: avg };
 }
@@ -10,17 +25,26 @@ function getArrayParams(arr) {
 // Задание 2
 function worker(arr) {
   let sum;
-
+  sum = 0;
+  for ( let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
   // Ваш код
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
+  let max; 
+  max = 0;
   // Ваш кода
   // for ...
+  for ( let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (max < sum) {
+      max = sum
+    };
+  };
   
   return max;
 }
@@ -28,4 +52,19 @@ function makeWork(arrOfArr, func) {
 // Задание 3
 function worker2(arr) {
   // Ваш код
+  let max = - Infinity;
+  let min = Infinity;
+
+  for ( let i = 0; i < arr.length; i++ ) {
+    if ( arr[i] > max) {
+      max = arr [i];
+    };
+    if ( arr[i] < min ) {
+      min = arr[i];
+    };
+  };
+
+  let differenceOfNumbers = Math.abs(max - min);
+  return differenceOfNumbers;
+
 }
