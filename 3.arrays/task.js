55217d505d601f286974db3df03db43b1b7f9b98
Ashamovskiy <476136@bk.ru>
2022-11-03 23:27:04 +0300

    Solved the Task 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,8 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])
+  
   return result; // boolean
 }
 
@@ -10,6 +11,8 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter(value => value > 0).filter(value => value % 3 === 0).map(item => item * 10)
+
 
   return resultArr; // array
 }
