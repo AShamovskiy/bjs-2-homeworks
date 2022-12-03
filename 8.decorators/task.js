1b33f7247910aebe69fb5b801a2044207e67af8a
Ashamovskiy <476136@bk.ru>
2022-12-03 23:03:50 +0300

    Solved for task 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,47 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
+
+  function wrapper (... args) {
+    let hash = args.join(',');
+    let hashInCache = cache.find((item) => item.hash === hash);
+    
+    if (hashInCache) {
+      console.log(`Из кеша:${hashInCache.result}`);
+      return `Из кеша:${hashInCache.result}`;
+    }
+
+    let result = func(...args);
+    cache.push({hash, result});
+    if (cache.length > 5) {
+      cache.shift();
+    };
+
+    console.log('Вычисляем:' +result);
+    return 'Вычисляем:' +result;
+  };
+
+  return wrapper
+};
 
 
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func, delay) {
   // Ваш код
-}
+  timerId = null;
+
+  function wrapper (...args) {
+    if (timerId) {
+      clearTimeout(timerId)
+    };
+
+    wrapper.allCount++
+    timerId = setTimeout(() => {
+      wrapper.count++
+      timerid = null;
+      return func(...args)
+    }, delay);
+  };
+
+  wrapper.count = [];
+  wrapper.allCount= [];
+  return wrapper;
+};
